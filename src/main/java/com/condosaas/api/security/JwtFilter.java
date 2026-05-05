577dfb3377436152287852bf7c79ee45de44df19
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:42:51 -0500

    Register JwtFilter as Spring component

--- src/main/java/com/condosaas/api/security/JwtFilter.java
+++ src/main/java/com/condosaas/api/security/JwtFilter.java
@@ -1,6 +1,8 @@
 package com.condosaas.api.security;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
 }
