3087ad1fc2fdb4d91171d3a70bcabdb5b758f5ce
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:45:14 -0500

    Override doFilterInternal method in JwtFilter

--- src/main/java/com/condosaas/api/security/JwtFilter.java
+++ src/main/java/com/condosaas/api/security/JwtFilter.java
@@ -1,8 +1,13 @@
 package com.condosaas.api.security;
 
 import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -13,6 +18,13 @@ public class JwtFilter extends OncePerRequestFilter {
     public JwtFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
         this.jwtUtil = jwtUtil;
         this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
     }
 
 }
