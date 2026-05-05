0ee4418364a6cab95951f50abb743351802897c6
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:10:36 -0500

    Extend UserRepository from JpaRepository

--- src/main/java/com/condosaas/api/repository/UserRepository.java
+++ src/main/java/com/condosaas/api/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.condosaas.api.repository;
 
-public interface UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
