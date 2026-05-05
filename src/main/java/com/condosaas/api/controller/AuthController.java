e06472d4765d090f3c156b84ca8285ab10f2537a
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 12:04:02 -0500

    Inject AuthenticationManager and JwtUtil into AuthController

--- src/main/java/com/condosaas/api/controller/AuthController.java
+++ src/main/java/com/condosaas/api/controller/AuthController.java
@@ -1,10 +1,20 @@
 package com.condosaas.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.condosaas.api.security.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
 }
