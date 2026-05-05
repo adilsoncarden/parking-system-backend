03c40e56e8eeb544872012e6d1690c9a26cd627c
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:43:50 -0500

    Inject JwtUtil and userDetailsService into JwtFilter

--- src/main/java/com/condosaas/api/security/JwtFilter.java
+++ src/main/java/com/condosaas/api/security/JwtFilter.java
@@ -1,9 +1,18 @@
 package com.condosaas.api.security;
+
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
-}
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
 
+    public JwtFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+}
