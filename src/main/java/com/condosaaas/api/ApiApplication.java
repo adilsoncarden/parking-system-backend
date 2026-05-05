5f900be8411e3606f96c82ba530a4ef69c6556b5
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 10:47:36 -0500

    refactor: rename base package to com.condosaas.api and rename models to entities

--- src/main/java/com/condosaaas/api/ApiApplication.java
+++ src/main/java/com/condosaaas/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.condosaaas.backend;
+package com.condosaaas.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
