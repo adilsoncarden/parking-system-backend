8fcdf49028aee746b5fe14517b73e14bb97f0394
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:37:52 -0500

    Implement loading user by username

--- src/main/java/com/condosaas/api/service/UserDetailsServiceImpl.java
+++ src/main/java/com/condosaas/api/service/UserDetailsServiceImpl.java
@@ -1,8 +1,12 @@
 package com.condosaas.api.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import com.condosaas.api.repository.UserRepository;
+import com.condosaas.api.entity.User;
+import java.util.Optional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -11,6 +15,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
     }
 
 }
