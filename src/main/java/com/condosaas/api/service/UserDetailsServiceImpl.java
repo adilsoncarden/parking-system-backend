039ab1a44d67db7929d5d17c5d92884e7a817337
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:36:00 -0500

    Inject UserRepository into UserDetailsServiceImpl

--- src/main/java/com/condosaas/api/service/UserDetailsServiceImpl.java
+++ src/main/java/com/condosaas/api/service/UserDetailsServiceImpl.java
@@ -2,8 +2,15 @@ package com.condosaas.api.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import com.condosaas.api.repository.UserRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-    
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 }
