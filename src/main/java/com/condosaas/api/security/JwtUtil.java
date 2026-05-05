849ef0c15c1c85e8c80e40fd1dddbb5329da02aa
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:27:53 -0500

    Implement JWT token generation

--- src/main/java/com/condosaas/api/security/JwtUtil.java
+++ src/main/java/com/condosaas/api/security/JwtUtil.java
@@ -3,11 +3,11 @@ package com.condosaas.api.security;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.function.Function;
 import java.util.Date;
-
 
 @Component
 public class JwtUtil {
@@ -44,6 +44,16 @@ public class JwtUtil {
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
     }
 
 }
