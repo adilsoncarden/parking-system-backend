91d58405518c713aac8f950de96553fa57509ffc
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 10:59:00 -0500

    Add id field to Role entity

--- src/main/java/com/condosaas/api/entity/Role.java
+++ src/main/java/com/condosaas/api/entity/Role.java
@@ -1,9 +1,17 @@
 package com.condosaas.api.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
 }
