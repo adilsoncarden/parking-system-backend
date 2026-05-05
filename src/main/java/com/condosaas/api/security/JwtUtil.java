ce01667ff1226536fb36c51ad4ad797c6f22206c
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:22:51 -0500

    Generate cryptographic key to sign JWT

--- src/main/java/com/condosaas/api/security/JwtUtil.java
+++ src/main/java/com/condosaas/api/security/JwtUtil.java
@@ -1,6 +1,9 @@
 package com.condosaas.api.security;
 
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+
 
 @Component
 public class JwtUtil {
@@ -8,5 +11,7 @@ public class JwtUtil {
     private final String SECRET_KEY = "EstaEsUnaClaveSuperSecretaParaCondoSaaS2026_DebeSerLarga";
 
     private final long JWT_TOKEN_VALIDITY = 1000 * 60 * 60 * 10;
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
 }
