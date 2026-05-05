95ec575f28938b9942a114ae5ccf658aca085dde
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:07:26 -0500

    Add Lombok to User entity

--- src/main/java/com/condosaas/api/entity/User.java
+++ src/main/java/com/condosaas/api/entity/User.java
@@ -1,16 +1,11 @@
 package com.condosaas.api.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
 
     @Id
