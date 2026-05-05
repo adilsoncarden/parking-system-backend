b40938eba6b3a2e5a054db5dc9f3beaca4a14e25
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:11:02 -0500

    Link UserRepository with User entity

--- src/main/java/com/condosaas/api/repository/UserRepository.java
+++ src/main/java/com/condosaas/api/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.condosaas.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.condosaas.api.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
