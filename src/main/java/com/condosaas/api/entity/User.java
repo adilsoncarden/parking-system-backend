cb6516fc2df4f1592923eb3bcde43c9f335ab777
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:04:42 -0500

    Add id field to User entity

--- src/main/java/com/condosaas/api/entity/User.java
+++ src/main/java/com/condosaas/api/entity/User.java
@@ -2,9 +2,16 @@ package com.condosaas.api.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 
 @Entity
 @Table(name = "users")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
