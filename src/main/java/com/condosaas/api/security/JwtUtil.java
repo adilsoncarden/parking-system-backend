88f8d8f71e2f92575b8a542c4b5894b950a75338
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:21:05 -0500

    Define JWT secret key and token validity

--- src/main/java/com/condosaas/api/security/JwtUtil.java
+++ src/main/java/com/condosaas/api/security/JwtUtil.java
@@ -4,5 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
-    
+
+    private final String SECRET_KEY = "EstaEsUnaClaveSuperSecretaParaCondoSaaS2026_DebeSerLarga";
+
+    private final long JWT_TOKEN_VALIDITY = 1000 * 60 * 60 * 10;
+
 }
