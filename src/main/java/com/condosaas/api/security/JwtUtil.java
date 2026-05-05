ef8969b393a7beec71e2e3b8d3af187e1c9c7dd3
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:26:07 -0500

    Add methods to extract information from JWT

--- src/main/java/com/condosaas/api/security/JwtUtil.java
+++ src/main/java/com/condosaas/api/security/JwtUtil.java
@@ -1,9 +1,10 @@
 package com.condosaas.api.security;
 
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
-
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -13,5 +14,20 @@ public class JwtUtil {
     private final long JWT_TOKEN_VALIDITY = 1000 * 60 * 60 * 10;
 
     private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(
+            String token,
+            Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
 
 }
