ada1b0a9bce295e93d9651c5e1de57b260cc24c8
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-05 11:45:47 -0500

    Read Authorization header and extract JWT

--- src/main/java/com/condosaas/api/security/JwtFilter.java
+++ src/main/java/com/condosaas/api/security/JwtFilter.java
@@ -25,6 +25,12 @@ public class JwtFilter extends OncePerRequestFilter {
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+        }
+
     }
 
 }
