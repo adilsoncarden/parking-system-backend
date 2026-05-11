ec5afacc27c44fcdb10c6edb0667efffc4eb2eae
fernando <fernandosanchez20044@gmail.com>
2026-05-11 03:04:45 -0500

    Corregiendo Controller  Torres

--- src/main/java/com/condosaas/api/torres/Controller/TorresController.java
+++ src/main/java/com/condosaas/api/torres/Controller/TorresController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "admin/torres")
+@RequestMapping(name = "/admin/torres")
 public class TorresController {
 
 
