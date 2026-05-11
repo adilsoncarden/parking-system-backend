2a6ee7c646b1278db71f4d94feb77f661005cf82
fernando <fernandosanchez20044@gmail.com>
2026-05-11 04:01:34 -0500

    feat:Corrigiendo servicios  de torres

--- src/main/java/com/condosaas/api/torres/service/TorresService.java
+++ src/main/java/com/condosaas/api/torres/service/TorresService.java
@@ -1,6 +1,8 @@
 package com.condosaas.api.torres.service;
 
 import com.condosaas.api.condominio.Repository.CondominioRepository;
+import com.condosaas.api.condominio.entity.Condominio;
+import com.condosaas.api.torres.dto.TorreRequest;
 import com.condosaas.api.torres.entity.Torres;
 import com.condosaas.api.torres.repository.TorresRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,27 +15,27 @@ import java.util.List;
 public class TorresService {
 
     private final TorresRepository torresRepository;
-    private final  CondominioRepository condominioRepository;
+    private final CondominioRepository condominioRepository;
 
- public List<Torres> ListarTodasLasTorresPorCondominio () {
-     return torresRepository.findAll();
+    public List<Torres> ListarTodasLasTorresPorCondominio() {
+        return torresRepository.findAll();
 
- }
+    }
+
+    public Torres guardarTorre(TorreRequest request) {
+        // 1. Buscamos el condominio real
+        Condominio con = condominioRepository.findById(request.getIdCondominio())
+                .orElseThrow(() -> new RuntimeException("Condominio no encontrado"));
+
+        // 2. Mapeamos del DTO a la Entidad
+        Torres torre = new Torres();
+        torre.setNombre(request.getNombre());
+        torre.setCantidadPisos(request.getPisos());
+        torre.setCantidadApartamentos(request.getAptos()); // Ojo con el nombre en tu entidad
+        torre.setCondominio(con);
+
+        return torresRepository.save(torre);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
