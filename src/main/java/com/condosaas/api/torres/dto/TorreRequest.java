1d50f8d98dfedf856641a8371a2e2cbc7748b0bf
fernando <fernandosanchez20044@gmail.com>
2026-05-11 04:00:20 -0500

    feat:Agregando DTO de torres

--- src/main/java/com/condosaas/api/torres/dto/TorreRequest.java
+++ src/main/java/com/condosaas/api/torres/dto/TorreRequest.java
@@ -1,4 +1,12 @@
 package com.condosaas.api.torres.dto;
 
+
+import lombok.Data;
+
+@Data
 public class TorreRequest {
+    private String nombre;
+    private Long idCondominio;
+    private Integer pisos;
+    private Integer aptos;
 }
